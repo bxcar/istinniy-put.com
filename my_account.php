69970ec608e2b57ba273681631c155d6945bc822
bxcar <malanchukdima@mail.ru>
2018-04-06 16:37:50 +0300

    form login fix

--- my_account.php
+++ my_account.php
@@ -21,7 +21,57 @@
               <?php echo do_shortcode("[wppb-recover-password]"); ?>
             </div><?php } else { ?><h4 class="autorization-login-title">Авторизация</h4>
             <div class="autorization-login">
-              <?php echo do_shortcode("[wppb-login]"); ?>
+                <?php if(is_user_logged_in()) {
+                    echo do_shortcode("[wppb-login]"); ?>
+                    <?php
+                    $user = wp_get_current_user();
+                    if(in_array( 'premium_user', (array) $user->roles )) { ?>
+                        <span class="subs-purchased">Ваша подписка активна, у вас есть доступ к премиум-контенту на сайте</span>
+                    <?php } ?>
+                <?php } else {
+//                    echo do_shortcode("[wppb-login]");
+                    wp_login_form(array(
+                        'echo'           => true,
+                        'label_username' => __( 'Логин' ),
+                        'label_password' => __( 'Пароль' ),
+                        'label_remember' => __( 'Запомнить меня' ),
+                        'label_log_in'   => __( 'Войти' ),
+                    ));
+                }?>
+                <style>
+                    #loginform .login-password label,
+                    #loginform .login-username label {
+                        width: 30%;
+                        float: left;
+                        min-height: 1px;
+                        text-align: center;
+                    }
+
+                    #loginform .login-password,
+                    #loginform .login-username {
+                        text-align: left;
+                        margin-bottom: 15px;
+                    }
+
+                    #loginform input {
+                        border: 1px solid #ccc;
+                        border-radius: 3px;
+                    }
+
+                    #loginform .login-remember {
+                        margin-top: 5px;
+                    }
+
+                    #loginform .login-submit {
+                        margin-top: 15px;
+                    }
+
+                    .subs-purchased {
+                        color: #288965;
+                        margin-top: 20px;
+                        display: inline-block;
+                    }
+                </style>
             </div><?php } ?>
             </div>
 
